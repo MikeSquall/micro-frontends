ff78c6b27a189de06894db8228f6cdc7c4a8906e
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-01-21 15:57:20 +0200

    wip

--- 4-scalecube-b2b/src/team-green/GreenRecos.js
+++ 4-scalecube-b2b/src/team-green/GreenRecos.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { BlueService } from '../api.js';
 import '../../dist/scalecube.js';
 import { RedService } from "../api.js";
@@ -5,33 +7,46 @@ import { RedService } from "../api.js";
 const microservices = scalecube.Microservices.builder().build();
 
 export const redService = microservices
-    .proxy()
-    .api(RedService)
-    .create();
+  .proxy()
+  .api(RedService)
+  .create();
 
 const recos = {
   t_porsche: ['3', '5', '6'],
   t_fendt: ['3', '6', '4'],
   t_eicher: ['1', '8', '7'],
 };
+
 const host = 'http://localhost:8080/src/';
 
-export class GreenRecos extends HTMLElement {
+class Recommendations extends React.Component {
+  render() {
+    console.log('lolo');
 
-  connectedCallback() {
-      redService
-          .listenProduct()
-          .subscribe((sku) => this.render(sku));
-
-  }
-  render(sku) {
-    const reco = recos[sku] || [];
-    this.innerHTML = `
+    return (
+      <>
       <h3>Related Products</h3>
-      ${reco.map(id => `<img src="${host}/team-green/images/reco_${id}.jpg" alt="Reco ${id}" />`).join('')}
-    `;
+      {this.props.recommendations.map(
+        id => <img src={`${host}/team-green/images/reco_${id}.jpg`} alt={`Reco ${id}`}/>
+      )}
+    </>;
   }
+}
+
+export class GreenRecos extends HTMLElement {
+  connectedCallback() {
+    redService
+      .listenProduct()
+      .subscribe((sku) => this.render(sku));
+  }
+
+  render(sku) {
+    console.log('A');
+    return <Recommendations recommendations={recos[sku] || []}/>;
+  }
+
   disconnectedCallback() {}
+
   log(...args) {
     console.log('🖼️ green-recos', ...args);
   }
