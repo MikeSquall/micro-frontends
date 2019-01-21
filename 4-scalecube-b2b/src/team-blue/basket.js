cee541e58e844f95585f7efa59b118b437735136
idokatz <idok.mail@gmail.com>
2019-01-21 13:59:21 +0200

    check push permission

--- 4-scalecube-b2b/src/team-blue/basket.js
+++ 4-scalecube-b2b/src/team-blue/basket.js
@@ -1,15 +1,17 @@
-import {Observable} from "./Observable.js";
+import { Observable } from "./Observable.js";
 
 const basket = [];
 
-let notify = ()=>{};
+let notify = () => {
+};
 
 export const addToBasket = (item) => {
-    basket.push(item);
-    notify(item);
+  basket.push(item);
+  notify(item);
 };
-export const listenBasketEvents = new Observable((obs)=>{
-    notify = (item) => obs.next(item);
+
+export const listenBasketEvents = new Observable((obs) => {
+  notify = (item) => obs.next(item);
 });
 
 export const basketItems = () => [...basket];
